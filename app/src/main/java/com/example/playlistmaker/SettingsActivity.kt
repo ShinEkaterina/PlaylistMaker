7a60a77cb5c0188540f2820fe8d451b83150e894
Practicum <practicum@ya.ru>
2023-04-23 18:33:43 +0600

    write to support + show agreement

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,9 +2,9 @@ package com.example.playlistmaker
 
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 
@@ -15,12 +15,31 @@ class SettingsActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.settings_toolbar)
         toolbar.setNavigationOnClickListener { finish() }
 
-        var shareAppIcon = findViewById<ImageView>(R.id.share_app_button)
+        var shareAppIcon = findViewById<ImageView>(R.id.share_app_icon)
+        var supportIcon = findViewById<ImageView>(R.id.support_icon)
+        var agreementIcon = findViewById<ImageView>(R.id.go_to_agreement_icon)
+
+
 
         shareAppIcon.setOnClickListener {
             var intent = Intent(Intent.ACTION_SEND)
             intent.type = "text/plain"
             intent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/")
+            startActivity(intent)
+        }
+        supportIcon.setOnClickListener {
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("Shinus90@yandex.com"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(intent)
+        }
+        agreementIcon.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
             startActivity(intent)
         }
     }
